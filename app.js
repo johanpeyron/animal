d8633c0f9247914f09c5def0f1127a1056d824d5
Blixtbild <johan.peyron@gmail.com>
2018-06-01 14:56:38 +0200

    0.1.0

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require('express');
 const chalk = require('chalk');
 const path = require('path');
 const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.port || 3000;
 
-app.use(morgan(tiny));
-app.use(express.static(path.join(__dirname, public)));
-app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'ja')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
+app.use('/css', express.static(path.join(__dirname, 'public', 'js')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'));
 
 });
 
